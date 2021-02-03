e62d2d78f5b8ded64e9ea035d938cc0b16493213
rsykes2 <rsykes1947@gmail.com>
2021-02-03 14:36:56 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>El Ricardo's Shopify app with Node and React 🎉</Heading>
+  </Page>
+);
+
+export default Index;
