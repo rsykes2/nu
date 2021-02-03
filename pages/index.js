467cfcc661261a5828a454e7c0c988df1b0ffc3b
rsykes2 <rsykes1947@gmail.com>
2021-02-03 14:54:19 -0600

    sevent

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
-    <Heading>El Ricardo's Shopify app with Node and React 🎉</Heading>
+    <Heading>El Ricardo's Shopify app with Node and React 🎉 Hard to believe.</Heading>
   </Page>
 );
 
