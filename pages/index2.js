b5ba8b74712c79c3142b5e17d6defa4682a5d20f
rsykes2 <rsykes1947@gmail.com>
2021-02-06 21:54:38 -0600

    567

---
+++ pages/index2.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>El Ricardo's Shopify app with Node and React 🎉    It gets Much Harder and harder.But not too bad.</Heading>
+  </Page>
+);
+
+export default Index;
